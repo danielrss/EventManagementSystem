aa9ed66a5649bef154a6ec083aa91e00f4dd4cc2
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-11-27 00:14:03 +0200

    Homepage design

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,28 @@
-/**
- * Created by Dimitar on 21.11.2016 г..
- */
+'use strict';
+
+$(document).ready(function() {
+    animateLandingPageElements();
+});
+
+
+document.onreadystatechange = function () {
+    let state = document.readyState,
+        loader = document.getElementById('page-loader'),
+        content = document.getElementById('content');
+
+    if (state === 'interactive') {
+        content.style.visibility = 'hidden';
+    } else if (state === 'complete') {
+        setTimeout(function(){
+            loader.style.visibility = 'hidden';
+            content.style.visibility = 'visible';
+        },1000);
+    }
+};
+
+function animateLandingPageElements(){
+    $('#logo').addClass('animated rotateIn');
+    $('#hp-caption-title').addClass('animated fadeInUp');
+    $('#hp-caption-subtitle').addClass('animated fadeInUp');
+    $('#hp-caption-buttons').addClass('animated fadeInUp');
+}
