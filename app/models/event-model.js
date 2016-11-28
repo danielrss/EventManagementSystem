c7303319041bfc20c94eece18d0df6599860f590
Daniel Isov <danielisov96@gmail.com>
2016-11-28 20:44:03 +0200

    user section now available with updated header

--- app/models/event-model.js
+++ app/models/event-model.js
@@ -7,62 +7,60 @@ const mongoose = require('mongoose'),
 const letters = /[A-Za-z]/;
 
 let EventSchema = new Schema({
-   /* name: {
-        type: String,
-        required: true,
-        minlength: [3, 'Name is too short!'],
-        maxlength: [50, 'Name is too long!'],
-        match: letters
-    },
-    createdOn: {
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    eventType: {
-        _id: Number,
-        required: true
-    },
-    user: {
-        _id: Number,
-        name: String,
-        required: true
-    },
-    location: {
-        _id: Number,
-        address: String
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    dateOfEvent: {
-        type: Date,
-        required: true
-    },
-    cover: {
-        type: Buffer,
-        required: true
-    },
-    rating: {
-        type: Number,
-        required: true
-    },
-    isDeleted: {
-        type: Boolean
-    },
-    capacity: {
-        type: Number,
-        required: true
-    },
-    status: {
-        _id: Number,
-        type: String
-    }*/
+//     name: {
+//         type: String,
+//         required: true,
+//         minlength: [3, 'Name is too short!'],
+//         maxlength: [50, 'Name is too long!'],
+//         match: letters
+//     },
+//     createdOn: {
+//         type: Date,
+//         default: Date.now,
+//         required: true
+//     },
+//     eventType: {
+//         _id: Number,
+//         required: true
+//     },
+//     user: {
+//         _id: Number,
+//         name: String,
+//         required: true
+//     },
+//     location: {
+//         _id: Number,
+//         address: String
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     dateOfEvent: {
+//         type: Date,
+//         required: true
+//     },
+//     cover: {
+//         type: Buffer,
+//         required: true
+//     },
+//     rating: {
+//         type: Number,
+//         required: true
+//     },
+//     isDeleted: {
+//         type: Boolean
+//     },
+//     capacity: {
+//         type: Number,
+//         required: true
+//     },
+//     status: {
+//         _id: Number,
+//         type: String
+//     }
 });
 
 mongoose.model('Event', EventSchema);
-
 let Event = mongoose.model('Event');
-
 module.exports = Event;
--- app/routers/user-router.js
+++ app/routers/user-router.js
@@ -3,12 +3,12 @@
 module.exports = function(app, express, data) {
     let userRouter = new express.Router(),
         authController = require('../controllers/auth-controller')(data),
-        userController = require('../controllers/user-controller')();
-
+        userController = require('../controllers/user-controller')(data);
 
     userRouter
         .get('/login', userController.getLogin)
         .post('/login', authController.loginLocal)
+        .get('/logout', authController.logout)
         .get('/register', userController.getRegister)
         .post('/register', authController.register)
         .get('/profile', userController.getProfile)
