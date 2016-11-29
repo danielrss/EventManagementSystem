59c818c68ac40e8a11141b6661591a2e0274ee1e
loriyanaa <lsdoncheva@abv.bg>
2016-11-29 14:16:33 +0200

    Fixed event-model

--- app/models/event-model.js
+++ app/models/event-model.js
@@ -7,58 +7,61 @@ const mongoose = require('mongoose'),
 const letters = /[A-Za-z]/;
 
 let EventSchema = new Schema({
-//     name: {
-//         type: String,
-//         required: true,
-//         minlength: [3, 'Name is too short!'],
-//         maxlength: [50, 'Name is too long!'],
-//         match: letters
-//     },
-//     createdOn: {
-//         type: Date,
-//         default: Date.now,
-//         required: true
-//     },
-//     eventType: {
-//         _id: Number,
-//         required: true
-//     },
-//     user: {
-//         _id: Number,
-//         name: String,
-//         required: true
-//     },
-//     location: {
-//         _id: Number,
-//         address: String
-//     },
-//     description: {
-//         type: String,
-//         required: true
-//     },
-//     dateOfEvent: {
-//         type: Date,
-//         required: true
-//     },
-//     cover: {
-//         type: Buffer,
-//         required: true
-//     },
-//     rating: {
-//         type: Number,
-//         required: true
-//     },
-//     isDeleted: {
-//         type: Boolean
-//     },
-//     capacity: {
-//         type: Number,
-//         required: true
-//     },
-//     status: {
-//         _id: Number,
-//         type: String
-//     }
+    name: {
+        type: String,
+        required: true,
+        minlength: [3, 'Name is too short!'],
+        maxlength: [50, 'Name is too long!'],
+        match: letters
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    eventType: {
+        type: String,
+        _id: Number,
+        required: true
+    },
+    user: {
+        type: String,
+        _id: Number,
+        name: String,
+        required: true
+    },
+    location: {
+        type: String,
+        _id: String,
+        address: String
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    dateOfEvent: {
+        type: Date,
+        required: true
+    },
+    cover: {
+        type: Buffer,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    isDeleted: {
+        type: Boolean
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        _id: Number       
+    }
 });
 
 mongoose.model('Event', EventSchema);
