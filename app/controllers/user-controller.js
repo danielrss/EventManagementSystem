553f64f19e4ce5dd50c81e8506c58ae19ad4b8d7
Daniel Isov <danielisov96@gmail.com>
2016-11-30 15:44:35 +0200

    implemented functionality for uploading profile avatar, validation to be made...

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -1,5 +1,8 @@
 'use strict';
-const helpers = require('../helpers');
+const helpers = require('../helpers'),
+    formidable = require('formidable'),
+    fs = require('fs-extra'),
+    path = require('path');
 
 module.exports = function (data) {
     return {
@@ -21,6 +24,58 @@ module.exports = function (data) {
                     } else {
                         res.render('user/profile', { user: req.user });
                     }
+                });
+        },
+        getProfileAvatar(req, res) {
+            return Promise.resolve()
+                .then(() => {
+                    if (!req.isAuthenticated()) {
+                        res.status(401).redirect('/unauthorized');
+                    } else {
+                        res.render('user/profile-avatar', { user: req.user });
+                    }
+                });
+        },
+        uploadProfileAvatar(req, res) {
+            return Promise.resolve()
+                .then(() => {
+                    if (!req.isAuthenticated()) {
+                        res.status(401).redirect('/unauthorized');
+                    } else {
+                        let form = new formidable.IncomingForm();
+                        form.parse(req, function(err, fields, files) {
+                            res.status(200)
+                                .send({ redirectRoute: '/profile' });
+                        });
+
+                        form.on('end', function (fields, files) {
+                            /* Temporary location of our uploaded file */
+                            let tempPath = this.openedFiles[0].path;
+                            /* The file name of the uploaded file */
+                            let openedFileName = this.openedFiles[0].name;
+                            let newFileName = req.user.id + openedFileName.substring(openedFileName.lastIndexOf('.'), openedFileName.length);
+                            /* Location where we want to copy the uploaded file */
+                            let pathToNewFile = path.join(__dirname, '../../public/uploads/users', newFileName);
+
+                            fs.copy(tempPath, pathToNewFile, function (err) {
+                                if (err) {
+                                    console.error(err);
+                                }
+
+                                fs.remove(tempPath, (err) => {
+                                    if (err) {
+                                        console.error(err);
+                                    }
+
+                                    data.findUserByIdAndUpdate(req.user.id, { avatarUrl: '/static/uploads/users/' + newFileName });
+                                });
+                            });
+                        });
+                    }
+                })
+                .catch(err => {
+                    res.status(400)
+                        .send(JSON.stringify({ validationErrors: helpers.errorHelper(err) }));
                 });
         },
         getUnauthorized(req, res) {
