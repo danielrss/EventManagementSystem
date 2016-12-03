5819b9f07af4f75ca28b5d9ddf5958c901972744
zorie <shindova@abv.bg>
2016-12-03 23:48:30 +0200

    Datetime picker now available

--- public/scripts/events/rate-event.js
+++ public/scripts/events/rate-event.js
@@ -7,7 +7,7 @@
         $countOfLikes = $('.likes'),
         $countOfDislikes = $('.dislikes');
 
-    $likeBtn.on('click', function(event) {
+    $likeBtn.one('click', function(event) {
         $dislikeBtn.removeClass('btn-primary');
         $likeBtn.addClass('btn-primary');
 
@@ -37,7 +37,7 @@
             });
     });
 
-    $dislikeBtn.on('click', function(event) {
+    $dislikeBtn.one('click', function(event) {
         $dislikeBtn.addClass('btn-primary');
         $likeBtn.removeClass('btn-primary');
 
